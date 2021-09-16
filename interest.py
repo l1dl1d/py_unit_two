464e3a491e00cf81c3c4f4ad8f08fe9c97c9444f
l1dl1d <javeekh@ssfs.org>
2021-09-16 09:54:59 -0400

    finished interst

--- interest.py
+++ interest.py
@@ -1,2 +1,8 @@
 # Use this file to calculate the amount of compound interest. Follow the instructions on the
 # Variables and Expressions sheet carefully.
+p = 10000
+n = 12
+r =0.08
+t = 10
+
+print(p*(1+r/n)**(n*t))
--- sphere.py
+++ sphere.py
@@ -1,2 +1,5 @@
 # Follow the instructions on the Variables and Expressions sheet to calculate the volume of a sphere with
 # a given radius. As a hint, when doing something to the 3rd power, Python has a special operator. r^3 will NOT work.
+radius = 5
+pi = 3.14
+print(4/3*pi*5**3)
