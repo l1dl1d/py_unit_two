d1ee88d4ae564aa333cb982d8144853871dcf842
l1dl1d <javeekh@ssfs.org>
2021-09-24 09:01:45 -0400

    finished chat-bot

--- assignment_two.py
+++ assignment_two.py
@@ -12,5 +12,4 @@ yearly_interest_on_the_car = input("whats the yearly interest on a car like that
 years = input("if you had to take a loan to buy the "+ dream_car+ " how many years would you take the loan out for?")
 r = float(yearly_interest_on_the_car)/100/12
 n = int(years)*12
-
 print("if you had bought the", dream_car,"you would have a monthly payment of $",(r*float(cost_of_dream_car))/(1-(1+r)**-n), ",hopefully that's reasonable for your budget that's a total of $",(r*float(cost_of_dream_car))/(1-(1+r)**-n)*60)
